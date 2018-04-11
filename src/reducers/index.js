d5231a30f1c81544d3ce29fa21cdcfd2f9d9e95a
Carlos Gomez <gomezmendozacarlos@gmail.com>
2018-04-11 17:23:05 -0500

    new folderes added

---
+++ src/reducers/index.js
@@ -0,0 +1,9 @@
+const initialState = {
+    message: "it's easy to integrate React and Redux, isn't it?"
+}
+
+function reducer(state = initialState) {
+    return state;
+}
+
+export default reducer;
